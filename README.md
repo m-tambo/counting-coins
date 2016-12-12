e7895c27f61bb6ba0be01264c4001d2fe57fec25
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-12 08:51:48 -0600

    trying a few things

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-exercise-counting-coins
+
+###Write a program that will convert a dollar amount to the number of coins that make up the amount. Use the common American coins of quarters, dimes, nickels, and pennies.
--- coins.js
+++ coins.js
@@ -9,15 +9,47 @@
   }
 
 */
+console.log("is this thing on?")
+var q
+var d
+var n
+var p
+var coinPurse = {quarters: q, dimes: d, nickels: n, pennies: p}
+var amountInput = document.getElementById("amount").value
+console.log(coinPurse)
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+// listen for enter key in text input and run coinCounter
+document.getElementById("amount").addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    console.log("hi")
+    // coinCounter
+  }
+})
 
-  coinPurse.quarters = 0;
+// function coinCounter (q, d, n, p) {
+//   // Initialize a JavaScript object to hold the coins
+//   var coinPurse = {
+//     quarters: q,
+//     dimes: d,
+//     nickels: n,
+//     pennies: p
+//   };
 
-  return coinPurse;
-}
+//   coinPurse.quarters = 0;
 
-var coins = coinCounter()
-console.log();
+//   console.log(coinPurse)
+//   document.getElementById("amount").innerHTML = coinPurse
+// }
+
+// coinCounter
+// var coins = coinCounter()
+// console.log(coinPurse);
+
+// coinPurse = `
+//       <p>Quarters: ${q}</p>
+//       <p>Dimes: ${d}</p>
+//       <p>Nickels: ${n}</p>
+//       <p>Pennies: ${p}</p>
+//             `
+
+// document.getElementById("amount").innerHTML = coinPurse
